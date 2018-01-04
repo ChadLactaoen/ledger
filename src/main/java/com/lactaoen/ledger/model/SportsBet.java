b9acab62d85514b6fa651f4d01b9de2c67e0a961
Chad Lactaoen <chadlactaoen@gmail.com>
2018-01-03 23:28:13 -0800

    Adding edit period button, setting up Sports Bets

---
+++ src/main/java/com/lactaoen/ledger/model/SportsBet.java
@@ -0,0 +1,62 @@
+package com.lactaoen.ledger.model;
+
+public class SportsBet extends Bet {
+
+    private Integer odds;
+    private Team forTeam;
+    private Team againstTeam;
+    private Integer line;
+    private BetType betType;
+    private boolean isLive;
+
+    public SportsBet() {
+    }
+
+    public Integer getOdds() {
+        return odds;
+    }
+
+    public void setOdds(Integer odds) {
+        this.odds = odds;
+    }
+
+    public Team getForTeam() {
+        return forTeam;
+    }
+
+    public void setForTeam(Team forTeam) {
+        this.forTeam = forTeam;
+    }
+
+    public Team getAgainstTeam() {
+        return againstTeam;
+    }
+
+    public void setAgainstTeam(Team againstTeam) {
+        this.againstTeam = againstTeam;
+    }
+
+    public Integer getLine() {
+        return line;
+    }
+
+    public void setLine(Integer line) {
+        this.line = line;
+    }
+
+    public BetType getBetType() {
+        return betType;
+    }
+
+    public void setBetType(BetType betType) {
+        this.betType = betType;
+    }
+
+    public boolean isLive() {
+        return isLive;
+    }
+
+    public void setLive(boolean live) {
+        isLive = live;
+    }
+}
---
+++ src/main/java/com/lactaoen/ledger/model/Team.java
@@ -0,0 +1,26 @@
+package com.lactaoen.ledger.model;
+
+public class Team {
+
+    private Integer teamId;
+    private String teamName;
+
+    public Team() {
+    }
+
+    public Integer getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Integer teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+}
